ac53ca94ce601e3b9dbffd0e4501032e18e5b2ae
shenruiqin <shenruiqin@ecaray.com>
2018-02-27 15:51:10 +0800

    优化

--- app/src/main/java/com/ruiqin/intercepthomedemo/BaseApplication.java
+++ app/src/main/java/com/ruiqin/intercepthomedemo/BaseApplication.java
@@ -107,8 +107,7 @@ public class BaseApplication extends Application {
             if (rti.topActivity.getPackageName().equals(context.getPackageName())) {
                 try {
                     Intent resultIntent = new Intent(context, Class.forName(rti.topActivity.getClassName()));
-                    resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, resultIntent, 0);
+                    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_ONE_SHOT);
                     pendingIntent.send();
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
@@ -128,5 +127,24 @@ public class BaseApplication extends Application {
         } catch (PendingIntent.CanceledException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public void backForeground2() {
+        //获取ActivityManager
+        ActivityManager mAm = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        //获得当前运行的task
+        List<ActivityManager.RunningTaskInfo> taskList = mAm.getRunningTasks(100);
+        for (ActivityManager.RunningTaskInfo rti : taskList) {
+            //找到当前应用的task，并启动task的栈顶activity，达到程序切换到前台
+            if (rti.topActivity.getPackageName().equals(context.getPackageName())) {
+                mAm.moveTaskToFront(rti.id, ActivityManager.MOVE_TASK_WITH_HOME);
+                return;
+            }
+        }
+        //若没有找到运行的task，用户结束了task或被系统释放，则重新启动mainactivity
+        Intent resultIntent = new Intent(context, MainActivity.class);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(resultIntent);
     }
 }
